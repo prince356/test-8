9d85889b347dd1a5ba2e1be1e727b1ac621b4526
prince356 <princechaurasia014@gmail.com>
2025-03-19 18:08:10 +0530

    UC3: Use Lombok for Logging in AddressBookController

--- src/main/java/com/example/addressbook/controller/AddressBookController.java
+++ src/main/java/com/example/addressbook/controller/AddressBookController.java
@@ -1,35 +1,42 @@
 package com.example.addressbook.controller;
 
 import com.example.addressbook.dto.AddressBookDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/addressbook")
+@Slf4j
 public class AddressBookController {
 
     @GetMapping("")
     public ResponseEntity<String> getAddressBook() {
+        log.info("Fetching all address book data");
         return ResponseEntity.ok("Address Book Data");
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<String> getAddressBookById(@PathVariable int id) {
+        log.info("Fetching address book data for ID: {}", id);
         return ResponseEntity.ok("Address Book Data for ID: " + id);
     }
 
     @PostMapping("")
     public ResponseEntity<String> createAddressBook(@RequestBody AddressBookDTO addressBookDTO) {
+        log.info("Creating new address book entry: {}", addressBookDTO);
         return ResponseEntity.ok("Address Book Created: " + addressBookDTO.toString());
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<String> updateAddressBook(@PathVariable int id, @RequestBody AddressBookDTO addressBookDTO) {
+        log.info("Updating address book entry for ID: {} with data: {}", id, addressBookDTO);
         return ResponseEntity.ok("Address Book Updated for ID: " + id + " with Data: " + addressBookDTO.toString());
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteAddressBook(@PathVariable int id) {
+        log.info("Deleting address book entry for ID: {}", id);
         return ResponseEntity.ok("Address Book Deleted for ID: " + id);
     }
 }
